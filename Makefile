946cf123384e3817a8fb92495d3d16aa197e2865
Cem Cebeci <cemcebeci99@gmail.com>
2024-02-28 14:54:17 +0100

    delete old folders

--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	$(MAKE) -C rlc && cp rlc/build/* build
-	$(MAKE) -C open_spiel && cp open_spiel/build/* build
-clean:
-	rm -f build/* && $(MAKE) -C open_spiel clean && $(MAKE) -C rlc clean
--- crazy_eights/measure.py
+++ crazy_eights/measure.py
@@ -3,9 +3,9 @@ from statistics import mean
 import time
 import matplotlib.pyplot as plt
 
-REPEAT_COUNT = 2
-fuzzers = ['open_spiel_whitebox_crazy_eights']
-bug_depths = range(7, 26, 2)
+REPEAT_COUNT = 4
+fuzzers = ['open_spiel_whitebox_crazy_eights', 'open_spiel_blackbox_crazy_eights', 'rlc_crazy_eights', 'rlc_crazy_eights_no_fsm', 'rlc_crazy_eights_no_precons', 'rlc_crazy_eights_no_improvements']
+bug_depths = range(1, 20, 2)
 
 time_measurements = {fuzzer: {} for fuzzer in fuzzers}
 execution_count_measurements = {fuzzer: {} for fuzzer in fuzzers}
@@ -21,12 +21,16 @@ for bug_depth in bug_depths:
         execution_count_measurements[fuzzer][bug_depth] = []
         print(f"{bug_depth} - {fuzzer}")
         for i in range(REPEAT_COUNT):
-            start = time.time()
-            sp = run([f'build/{fuzzer}','-max_total_time=150', '-print_final_stats=1'], capture_output=True)
-            end = time.time()
-            time_measurements[fuzzer][bug_depth].append(end - start)
-            execution_count = (int)(sp.stderr.decode().splitlines()[-5].split()[-1])
-            execution_count_measurements[fuzzer][bug_depth].append(execution_count)
+            if j > 1 and time_measurements[fuzzer][bug_depths[j - 2]][0] > 150: # shortcut
+                time_measurements[fuzzer][bug_depth].append(150)
+                execution_count_measurements[fuzzer][bug_depth].append(0)
+            else: 
+                start = time.time()
+                sp = run([f'build/{fuzzer}','-artifact_prefix=fuzzer_output/','-max_total_time=150', '-print_final_stats=1'], capture_output=True)
+                end = time.time()
+                time_measurements[fuzzer][bug_depth].append(end - start)
+                execution_count = (int)(sp.stderr.decode().splitlines()[-5].split()[-1])
+                execution_count_measurements[fuzzer][bug_depth].append(execution_count)
 
     processed_depths = bug_depths[:j]
     print(processed_depths)
@@ -36,11 +40,11 @@ for bug_depth in bug_depths:
     average_count_measurements = {fuzzer:[mean(execution_count_measurements[fuzzer][bug_depth]) for bug_depth in processed_depths] for fuzzer in fuzzers}
 
     fig, ax = plt.subplots() 
-    #ax.plot(processed_depths, average_time_measurements['rlc_tic_tac_toe'], 'g-', label='rlc-full')
-    #ax.plot(processed_depths, average_time_measurements['rlc_tic_tac_toe_no_fsm'], 'b-', label='rlc-no-fsm')
-    # ax.plot(processed_depths, average_time_measurements['rlc_tic_tac_toe_no_precons'], 'c-', label='rlc-no-precons')
-    # ax.plot(processed_depths, average_time_measurements['rlc_tic_tac_toe_no_improvements'], 'y-', label='rlc-blackbox')
-    # ax.plot(processed_depths, average_time_measurements['open_spiel_blackbox_tic_tac_toe'], 'r-', label='os-blackbox')
+    ax.plot(processed_depths, average_time_measurements['rlc_crazy_eights'], 'g-', label='rlc-full')
+    ax.plot(processed_depths, average_time_measurements['rlc_crazy_eights_no_fsm'], 'b-', label='rlc-no-fsm')
+    ax.plot(processed_depths, average_time_measurements['rlc_crazy_eights_no_precons'], 'c-', label='rlc-no-precons')
+    ax.plot(processed_depths, average_time_measurements['rlc_crazy_eights_no_improvements'], 'y-', label='rlc-blackbox')
+    ax.plot(processed_depths, average_time_measurements['open_spiel_blackbox_crazy_eights'], 'r-', label='os-blackbox')
     ax.plot(processed_depths, average_time_measurements['open_spiel_whitebox_crazy_eights'], 'm-', label='os-whitebox')
 
     plt.xlabel('Bug Depth')
@@ -56,11 +60,11 @@ for bug_depth in bug_depths:
             print("\t".join([str(measurement) for measurement in time_measurements[fuzzer][bug_depth]]))
 
     fig, ax = plt.subplots() 
-    # ax.plot(processed_depths, average_count_measurements['rlc_tic_tac_toe'], 'g-', label='rlc-full')
-    # ax.plot(processed_depths, average_count_measurements['rlc_tic_tac_toe_no_fsm'], 'b-', label='rlc-no-fsm')
-    # ax.plot(processed_depths, average_count_measurements['rlc_tic_tac_toe_no_precons'], 'c-', label='rlc-no-precons')
-    # ax.plot(processed_depths, average_count_measurements['rlc_tic_tac_toe_no_improvements'], 'y-', label='rlc-blackbox')
-    # ax.plot(processed_depths, average_count_measurements['open_spiel_blackbox_tic_tac_toe'], 'r-', label='os-blackbox')
+    ax.plot(processed_depths, average_count_measurements['rlc_crazy_eights'], 'g-', label='rlc-full')
+    ax.plot(processed_depths, average_count_measurements['rlc_crazy_eights_no_fsm'], 'b-', label='rlc-no-fsm')
+    ax.plot(processed_depths, average_count_measurements['rlc_crazy_eights_no_precons'], 'c-', label='rlc-no-precons')
+    ax.plot(processed_depths, average_count_measurements['rlc_crazy_eights_no_improvements'], 'y-', label='rlc-blackbox')
+    ax.plot(processed_depths, average_count_measurements['open_spiel_blackbox_crazy_eights'], 'r-', label='os-blackbox')
     ax.plot(processed_depths, average_count_measurements['open_spiel_whitebox_crazy_eights'], 'm-', label='os-whitebox')
     plt.yscale("log")
     plt.xlabel('Bug Depth')
